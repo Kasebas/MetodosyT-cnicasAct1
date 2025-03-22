38c77a84e79887e1249f7216b9332a538ab8628d
Kaseb <kasebas260598@gmail.com>
2025-03-22 21:23:07 +0100

    Clase cartas terminada con algunos errores

--- Parejas/src/Carta.java
+++ Parejas/src/Carta.java
@@ -1,2 +1,36 @@
-public class Carta {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Carta extends JButton {
+    private String imagen;
+    private boolean descubierta = false;
+
+    public Carta(String imagen) {
+        this.imagen = imagen;
+        setIcon(new ImageIcon("images/carta_oculta.png"));
+
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!descubierta) {
+                    setIcon(new ImageIcon(imagen));
+                    descubierta = true;
+                }
+            }
+        });
+    }
+
+    public void ocultar() {
+        setIcon(new ImageIcon("images/carta_oculta.png"));
+        descubierta = false;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public boolean estaDescubierta() {
+        return descubierta;
+    }
 }
